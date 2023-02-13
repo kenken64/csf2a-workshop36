6452990dc302ea18b588596f810e5bdb6cdcce21
KENNETH PHANG <bunnyppl@hotmail.com>
2023-02-13 15:14:13 +0800

    upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCityComponent } from './components/list-city.component';
+import { AddCityComponent } from './components/add-city.component';
+import { WeatherDetailsComponent } from './components/weather-details.component';
+
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCityComponent,
+    AddCityComponent,
+    WeatherDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
